e35c25442958f8bca3fa4cb65f1bf9912f07f452
Владимир <vkuznecov1504@mail.ru>
2024-02-02 14:45:40 +0400

    Создали метод который проверяет операционную систему и год выпуска устройства и выводит результат в консоль

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 
 public class Main {
 
@@ -9,14 +10,35 @@ public class Main {
         }
     }
 
+    public static void installingTheApplication(int clientOS, int clientDeviceYear) {
+        if (clientOS == 0) {
+            System.out.println("Установите версию для iOS по ссылке");
+            if (clientDeviceYear <= 2014) {
+                System.out.println("Установите облегченную версию для iOS по ссылке");
+            }
+        } else {
+            System.out.println("Установите версию для Android по ссылке");
+            if (clientDeviceYear <= 2014) {
+                System.out.println("Установите облегченную версию для Android по ссылке");
+            }
+        }
+    }
 
     public static void main(String[] args) {
         task1 ();
+        task2 ();
     }
 
     public static void task1(){
         System.out.println("Задание 1");
         int year = 2024;
         checkLeapYear(year);
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        int clientOS = 1;
+        int currentYear = LocalDate.now().getYear();
+        installingTheApplication(clientOS,currentYear);
     }
 }
