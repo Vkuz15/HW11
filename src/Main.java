5a41713476905f1ba7a73a47ec35a0816cc72acb
Владимир <vkuznecov1504@mail.ru>
2024-02-02 12:54:06 +0400

    Создали метод который проверяет високосный год или нет и выводит результат в консоль

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+
 public class Main {
+
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " Год является високосным");
+        } else {
+            System.out.println(year + " Год не является високосным");
+        }
+    }
+
+
     public static void main(String[] args) {
+        task1 ();
+    }
+
+    public static void task1(){
+        System.out.println("Задание 1");
+        int year = 2024;
+        checkLeapYear(year);
     }
 }
